25c330c47a7f68e8d345b468486296f172014456
Pierre Morin <ton_email_perso@exemple.com>
2026-09-23 11:56:10 +0200

    Squelette injection de dépendances

---
+++ PlanningRecette.Applications/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace planningRecette.Applications
+{
+    public static class DependencyInjection
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            assembly.GetTypes().Where(t => $"{assembly.GetName().Name}.Service" == t.Namespace
+                                        && !t.IsAbstract
+                                        && !t.IsInterface
+                                        && t.Name.EndsWith("Service"))
+            .Select(a => new { assignedType = a, serviceTypes = a.GetInterfaces().ToList() })
+            .ToList()
+            .ForEach(typesToRegister =>
+            {
+                typesToRegister.serviceTypes.ForEach(typeToRegister => services.AddScoped(typeToRegister, typesToRegister.assignedType));
+            });
+        }
+    }
+}
---
+++ planningRecette.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace planningRecette.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            assembly.GetTypes().Where(t => $"{assembly.GetName().Name}.Repository" == t.Namespace
+                                        && !t.IsAbstract
+                                        && !t.IsInterface
+                                        && t.Name.EndsWith("Repository"))
+            .Select(a => new { assignedType = a, serviceTypes = a.GetInterfaces().ToList() })
+            .ToList()
+            .ForEach(typesToRegister =>
+            {
+                typesToRegister.serviceTypes.ForEach(typeToRegister => services.AddScoped(typeToRegister, typesToRegister.assignedType));
+            });
+        }
+    }
+}
